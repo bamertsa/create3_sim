1378830c5b2ea26934f9844602be201ec8a8fd80
bamertsa <bamertsa@ethz.ch>
2024-03-16 21:46:55 +0100

    Undock

--- irobot_create_gazebo/irobot_create_gazebo_sim/src/sim.py
+++ irobot_create_gazebo/irobot_create_gazebo_sim/src/sim.py
@@ -4,10 +4,13 @@ import rclpy
 
 from rclpy.node import Node
 from rclpy.action import ActionClient
+from rclpy.duration import Duration
+from rclpy.time import Time
+
 
 from action_msgs.msg import GoalStatus
 
-from irobot_create_msgs.action import Dock, Undock, RotateAngle, DriveDistance
+from irobot_create_msgs.action import Dock, Undock, RotateAngle, DriveDistance, NavigateToPosition
 
 
 class robotHandler(Node):
@@ -16,13 +19,45 @@ class robotHandler(Node):
         super().__init__(str(namespace)+'_handler')
 
         #Action Clients
+        self._undock_action_client = ActionClient(self, Undock, str(namespace)+'/undock')
         self._rotate_angle_action_client = ActionClient(self, RotateAngle, str(namespace)+'/rotate_angle')
         self._drive_distance_action_client = ActionClient(self, DriveDistance, str(namespace)+'/drive_distance')
+        self._navigate_to_position_action_client = ActionClient(self, NavigateToPosition, str(namespace)+'/navigate_to_position')
 
         self.sim_counter = 0
 
+        self.starting_time = Time()
+
         self.get_logger().info(f'Action clients setup', once=True)
 
+    def sim_loop(self):
+
+        sim_duration = (Time() - self.starting_time)
+
+        if sim_duration < Duration(seconds=60):
+            if self.sim_counter == 0:
+                self.send_undock_goal()
+            elif (self.sim_counter % 2) == 1:
+                self.send_rotate_angle_goal(1.57)
+            else:
+                self.send_drive_distance_goal(2.0)
+        else:
+            self.get_logger().info('Simulation Finished!')
+            rclpy.shutdown()
+
+
+    def send_undock_goal(self):
+
+        self.get_logger().info('Waiting for action server...')
+        self._undock_action_client.wait_for_server()
+
+        goal_msg = RotateAngle.Goal()
+
+        self.get_logger().info('Sending undock request...')
+
+        self._send_goal_future = self._rotate_angle_action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def send_rotate_angle_goal(self, angle):
 
@@ -31,9 +66,9 @@ class robotHandler(Node):
 
         goal_msg = RotateAngle.Goal()
         goal_msg.angle = angle
-        goal_msg.max_rotation_speed = 0.5
+        goal_msg.max_rotation_speed = 1.0
 
-        self.get_logger().info('Sending goal request...')
+        self.get_logger().info('Sending roatate angle request...')
 
         self._send_goal_future = self._rotate_angle_action_client.send_goal_async(goal_msg)
 
@@ -42,15 +77,36 @@ class robotHandler(Node):
     def send_drive_distance_goal(self, distance):
 
         self.get_logger().info('Waiting for action server...')
-        self._rotate_angle_action_client.wait_for_server()
+        self._drive_distance_action_client.wait_for_server()
 
         goal_msg = DriveDistance.Goal()
         goal_msg.distance = distance
         goal_msg.max_translation_speed = 0.5
 
-        self.get_logger().info('Sending goal request...')
+        self.get_logger().info('Sending drive distance request...')
 
         self._send_goal_future = self._drive_distance_action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def send_naviagte_to_position_goal(self, distance):
+
+        self.get_logger().info('Waiting for action server...')
+        self._navigate_to_position_action_client.wait_for_server()
+
+        goal_msg = NavigateToPosition.Goal()
+        goal_msg.achieve_goal_heading = true
+        goal_msg.goal_pose.pose.position.x = self.x_start
+        goal_msg.goal_pose.pose.position.y = 0.0
+        goal_msg.goal_pose.pose.position.z = 0.0
+        goal_msg.goal_pose.pose.orientation.x = 0.0
+        goal_msg.goal_pose.pose.orientation.y = 0.0
+        goal_msg.goal_pose.pose.orientation.z = 0.0
+        goal_msg.goal_pose.pose.orientation.w = 1.0
+
+        self.get_logger().info('Sending navigate to position request...')
+
+        self._send_goal_future = self._navigate_to_position_action_client.send_goal_async(goal_msg)
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
@@ -74,25 +130,20 @@ class robotHandler(Node):
 
         self.sim_counter += 1
 
-        sim_loop(self)
+        self.sim_loop()
 
-def sim_loop(robot_handler):
-
-    if (robot_handler.sim_counter % 2) == 0:
-        robot_handler.send_rotate_angle_goal(1.57)
-    else:
-        robot_handler.send_drive_distance_goal(3.0)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    robot1_handler = robotHandler('robot1')
     robot2_handler = robotHandler('robot2')
+    robot2_handler.sim_loop()
 
-    sim_loop(robot1_handler)
-    sim_loop(robot2_handler)
-
+    robot1_handler = robotHandler('robot1')
+    robot1_handler.sim_loop()
     rclpy.spin(robot2_handler)
+    rclpy.spin(robot1_handler)
+    
 
 
 if __name__ == '__main__':
